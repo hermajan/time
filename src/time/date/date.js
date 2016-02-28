e2723784e486fc50cd8fc625c49cfd8bfc5e6e01
DJohnny <hermajan@gmail.com>
2016-02-28 19:38:02 +0100

    Changed writing of date.

--- src/time/date/date.js
+++ src/time/date/date.js
@@ -1,8 +1,11 @@
 /**
  * Writes date.
+ * @param date JavaScript Date object.
  * @return string Date in format [day].[month].[year].
  */
-function dateJS() {
-  var date=new Date();
-  document.write(date.getDate()+"."+(date.getMonth()+1)+"."+date.getFullYear());
+function writeDate(date) {
+	if(typeof date === "undefined") {
+		var date = new Date();
+	}
+	document.write(date.getDate()+"."+(date.getMonth()+1)+"."+date.getFullYear());
 }
